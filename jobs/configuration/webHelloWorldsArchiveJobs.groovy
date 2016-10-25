b2ce992cfd53f6b4c096bea62d8acf57783d147a
Daniel Ibañez <daniel.ibanez@softonic.com>
2016-10-25 16:26:28 +0200

    Adding folder to all jobs in configuration

--- jobs/configuration/webHelloWorldsArchiveJobs.groovy
+++ jobs/configuration/webHelloWorldsArchiveJobs.groovy
@@ -2,6 +2,10 @@ String home = "/tmp"
 String basePath = '1-configuration'
 GString customWorkspacePath = "${home}/workspaces/${basePath}"
 
+folder(basePath) {
+    description 'CI folder of web-hello-world'
+}
+
 job("$basePath/web-hello-world-archive") {
     customWorkspace(customWorkspacePath.toString())
     label('slave')
